745312a0ba65e0521b033e6f061f3127d8c66bca
fweni <fweni@student.wethinkcode.co.za>
2024-08-19 17:04:12 +0200

    two simple functions that store data on to local storage

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,50 +1,68 @@
-//to be edited for the interaction with the api
-function getDataFromAPI() {
-    const apiUrl = 'https://api.xbar.com/data';
+function storeUserData(account,privateKey) {
+    const encryptedKey = btoa(privateKey);
+    localStorage.setItem('userAccount',account);
+    localStorage.setItem('userPrivateKey', encryptedKey);
+}
+storeUserData('0x1d675kdasdfhx91234x','privatekey1');
 
-    fetch(apiUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log('Data received:', data);
-        })
-        .catch(error => {
-            console.error('There has been a problem with your fetch operation:', error);
-        });
+function getUserData() {
+    const acc = localStorage.getItem('userAccount')
+    const encryptedKey = localStorage.getItem('userPrivateKey');
+    const privateKey = encryptedKey ? atob(encryptedKey) : null;
+
+    return { acc, privateKey};
 }
 
-getDataFromAPI();
+const userData = getUserData();
+console.log(userData);
 
-function sendDataToAPI() {
-    const apiUrl = 'https://api.xbar.com/submit';
-    const dataToSend = {
-        key1: 'value1',
-        key2: 'value2',
-    };
+// //to be edited for the interaction with the api
+// function getDataFromAPI() {
+//     const apiUrl = 'https://api.xbar.com/data';
 
-    fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(dataToSend)
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log('Data successfully sent:', data);
-    })
-    .catch(error => {
-        console.error('There has been a problem with your fetch operation:', error);
-    });
-}
+//     fetch(apiUrl)
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error('Network response was not ok ' + response.statusText);
+//             }
+//             return response.json();
+//         })
+//         .then(data => {
+//             console.log('Data received:', data);
+//         })
+//         .catch(error => {
+//             console.error('There has been a problem with your fetch operation:', error);
+//         });
+// }
 
-sendDataToAPI();
+// getDataFromAPI();
+
+// function sendDataToAPI() {
+//     const apiUrl = 'https://api.xbar.com/submit';
+//     const dataToSend = {
+//         key1: 'value1',
+//         key2: 'value2',
+//     };
+
+//     fetch(apiUrl, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(dataToSend)
+//     })
+//     .then(response => {
+//         if (!response.ok) {
+//             throw new Error('Network response was not ok ' + response.statusText);
+//         }
+//         return response.json();
+//     })
+//     .then(data => {
+//         console.log('Data successfully sent:', data);
+//     })
+//     .catch(error => {
+//         console.error('There has been a problem with your fetch operation:', error);
+//     });
+// }
+
+// sendDataToAPI();
